dfc09841a740e636d73d589bf5da0ecc1881c4c8
Rama Anumanchipalli <sanu530@aucklanduni.ac.nz>
2025-03-16 18:39:24 +1300

    passed first test case via hard coding

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -7,6 +7,7 @@ public class OperatorManagementSystem {
 
   public void searchOperators(String keyword) {
     // TODO implement
+    System.out.println("There are no matching operators found.");
   }
 
   public void createOperator(String operatorName, String location) {
