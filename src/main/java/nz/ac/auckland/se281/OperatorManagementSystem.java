fc987c74dc30f7afef59ab53e6785b03f64ca6bb
Rama Anumanchipalli <sanu530@aucklanduni.ac.nz>
2025-04-21 16:10:06 +1200

    Passes task 2 test 11, reworked search activities to work better and to make output come out correctly

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -202,34 +202,45 @@ ArrayList<Operator> operators = new ArrayList<>();
 
   }
 
+  // SEARCH ACTIVITES STRARTS HERE ==========================
   public void searchActivities(String keyword) {
+
     // if the keyword is * we need to show all the activities
     if (keyword.trim().equals("*")) {
         int totalCount = 0; // This is to count how many there are to tell us 1,2,3 etc matching ....
+
         for(Operator op : operators) {
         ArrayList<Activity> activities = op.getActivities();
-            // lets go thru the activities within the arraylist and get their info
-        for (Activity activity : activities) {
+        totalCount += activities.size();
+        }
+
+          
+        if(totalCount == 0) {
+          MessageCli.ACTIVITIES_FOUND.printMessage("are", "no", "ies", ".");
+        } else {
+          String verb = (totalCount == 1) ? "is" : "are";
+          String plural = (totalCount == 1) ? "" : "ies";
+          MessageCli.ACTIVITIES_FOUND.printMessage(verb, String.valueOf(totalCount), plural, ":");
+          
+          // THIS WILL PRINT OUT THE ACTIVITIES
+          for(Operator op : operators) {
+          ArrayList<Activity> activities = op.getActivities();
+          for (Activity activity : activities) {
             String activityID = activity.getActivityID();
             String type = activity.getType().toString();
             String operatorName = op.returnOperator();
+
+            //Print out the message with activity name, id, type and operator name
             MessageCli.ACTIVITY_ENTRY.printMessage(activity.getName(), activityID, type, operatorName);
-            totalCount++;
           }
         } 
-            // if there is nothing
-            if (totalCount == 0) { 
-              MessageCli.ACTIVITIES_FOUND.printMessage("There are no matching activities found.");
-            } 
-            else {
-              String verb = (totalCount == 1) ? "is" : "are";
-              String plural = (totalCount == 1) ? "" : "s";
-              MessageCli.ACTIVITIES_FOUND.printMessage(verb, String.valueOf(totalCount), plural, ":");
-            }
-      }  else {
-        // This is for when keyword is not * , so we must look at name/type/op locaton
+      }
+
+      } else {
+        // This is for when keyword is not * , so we must look at name/type/op location
         int totalCount = 0;
         for(Operator op : operators) {
+
           ArrayList<Activity> activities = op.getActivities();
               // lets go thru the activities within the arraylist and get their info
           for (Activity activity : activities) {
@@ -239,31 +250,45 @@ ArrayList<Operator> operators = new ArrayList<>();
 
               if (activityName.contains(keyword.toLowerCase().trim()) || activityType.contains(keyword.toLowerCase().trim()) || opLocation.contains(keyword.toLowerCase().trim())) {
                 
-                String activityID = activity.getActivityID();
-                String type = activity.getType().toString();
-                String operatorName = op.returnOperator();
-                MessageCli.ACTIVITY_ENTRY.printMessage(activity.getName(), activityID, type, operatorName); 
-                totalCount++;
+                // String activityID = activity.getActivityID();
+                // String type = activity.getType().toString();
+                // String operatorName = op.returnOperator();
+                // MessageCli.ACTIVITY_ENTRY.printMessage(activity.getName(), activityID, type, operatorName); 
+                // totalCount++;
+              } 
+            }
+          }
+          // if there is nothing so , total count = 0 
+          if (totalCount == 0) { 
+            MessageCli.ACTIVITIES_FOUND.printMessage("are", "no", "ies", ".");
+          } 
+          else {
+
+            String verb = (totalCount == 1) ? "is" : "are";
+            String plural = (totalCount == 1) ? "" : "ies";
+            MessageCli.ACTIVITIES_FOUND.printMessage(verb, String.valueOf(totalCount), plural, ":");
+
+
+            // THIS WILL PRINT OUT THE ACTIVITIES
+            for(Operator op : operators) {
+              ArrayList<Activity> activities = op.getActivities();
+              for (Activity activity : activities) {
+                String activityName = activity.getName().toLowerCase();
+                String activityType = activity.getType().toString().toLowerCase();
+                String opLocation = op.getLocation().getFullName().toLowerCase();
+
+                if (activityName.contains(keyword.toLowerCase().trim()) || activityType.contains(keyword.toLowerCase().trim()) || opLocation.contains(keyword.toLowerCase().trim())) {
+                  String activityID = activity.getActivityID();
+                  String type = activity.getType().toString();
+                  String operatorName = op.returnOperator();
+                  MessageCli.ACTIVITY_ENTRY.printMessage(activity.getName(), activityID, type, operatorName); 
+                }
               }
-      }
-    
-    }
-     // waht if there is nothing even with normal keyword
-     if(totalCount ==0) 
-     MessageCli.ACTIVITIES_FOUND.printMessage("There are no matching activities found.");
-     else {
-      String verb = (totalCount == 1) ? "is" : "are";
-      String plural = (totalCount == 1) ? "" : "s";
-      MessageCli.ACTIVITIES_FOUND.printMessage(verb, String.valueOf(totalCount), plural, ":");
-     }
-  }
-  }
-  
-
-  
-     
-  
-
+            }
+          }
+        }
+      }  
+//SEARCH ACTIVITES ENDS HERE ==========================	
 
   
 
